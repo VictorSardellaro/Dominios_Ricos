caac084c5ea842cf94fc0c4c71a092e56054f4b2
VictorSardellaro <victor.henryk@outlook.com>
2021-07-05 16:02:39 -0300

    Fail_

--- PaymentContext/PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext/PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,9 +1,12 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -28,5 +31,14 @@ namespace PaymentContext.Domain.Commands
         public string Country { get; set; }
         public string Zipcode { get; set; }
 
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+            .Requires()
+            .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+            .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+            .HasMinLen(FirstName, 40, "Name.FirstName", "Nome deve conter até 40 caracteres")
+            );
+        }
     }
 }
